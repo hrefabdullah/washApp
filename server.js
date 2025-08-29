9caa402fa01067580e342aa44de468dcc23bb407
Abdullah <aa3541900@gmail.com>
2025-08-30 04:30:16 +0530

    react frontend

--- server.js
+++ server.js
@@ -1,27 +1,28 @@
 import express from 'express';
 import { Server } from 'socket.io';
 import http from 'http';
+import cors from 'cors';
 
 const app = express()
+app.use(cors())
 app.use(express.json())
-app.use(express.static('public'))
 
 const server = http.createServer(app)
 const io = new Server(server)
 
 io.on('connection', (socket) => {
-    console.log('a user connected')
+    console.log(`a user connected ${socket.id}`)
 
-    socket.on('chat_message', (msg) => {
-        io.emit('chat_message', msg)
+    socket.on('chat-message', (msg) => {
+        io.emit('chat-message', msg)
+    })
+
+    socket.on('chat-message', (msg) => {
+        console.log("Received message:", msg)
     })
 })
 
 
-app.get('/', (req, res) => {
-    res.sendFile('/public/index.html')
-})
-
 server.listen(8000, () => {
-    console.log('server is running on port http://localhost:8000')
+    console.log('server is running on http://localhost:8000')
 })
